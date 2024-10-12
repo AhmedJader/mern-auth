8819461bd58783d3ff3946cc710664d3cb66f843
Ahmed <ahmedabduljader@trentu.ca>
2024-10-12 18:51:42 -0400

    added .jsx routing pages and updated index title

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/home';
+import About from './pages/about';
+import Profile from './pages/profile';
+import Signup from './pages/signup';
+import Signin from './pages/Signin';
 
 export default function App() {
   return (
-    <h1 className='text-red-500'>App</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/home" element ={<Home />} />
+        <Route path="/about" element={<About />}/>
+        <Route path="/profile" element={<Profile />}/>
+        <Route path="/signup" element={<Signup />}/>
+        <Route path="/signin" element={<Signin />}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
